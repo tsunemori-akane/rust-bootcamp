8e0f29878685480671db991cbce220eddafbb982
tsunemori-akane <po6271933@outlook.com>
2026-03-11 23:23:47 +0800

    move to rcli file & add output.json

--- 1-rcli/src/main.rs
+++ 1-rcli/src/main.rs
@@ -1,20 +1,27 @@
 use anyhow::Result;
 use clap::Parser;
 use csv::Reader;
-use serde::Deserialize;
-use std::path::Path;
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 #[allow(dead_code)]
 struct Record {
-    #[serde(rename = "final_rankings__standing_player")]
+    #[serde(rename(
+        serialize = "final_rankings",
+        deserialize = "final_rankings__standing_player"
+    ))]
     final_rankings: String,
+
     nationality: String,
+
     #[serde(rename = "standing_player2")]
     player_name: String,
-    #[serde(rename = "final_rankings__age")]
+
+    #[serde(rename(serialize = "age", deserialize = "final_rankings__age"))]
     age: u32,
+
     #[serde(rename = "final_rankings__points")]
     rankings_points: u32,
 }
@@ -47,15 +54,20 @@ pub struct CsvOpts {
     pub header: bool,
 }
 // cargo run -- csv -i test.csv
+// cargo run csv -i assets/final_rankings_2021.csv
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
     match opts.cmd {
         SubCommand::Csv(opts) => {
             let mut reader = Reader::from_path(opts.input)?;
+            let mut ret = Vec::with_capacity(128);
             for result in reader.deserialize::<Record>() {
                 let record = result?;
-                println!("{:?}", record);
+                // println!("{:?}", record);
+                ret.push(record);
             }
+            let json = serde_json::to_string_pretty(&ret)?;
+            fs::write(&opts.output, json)?;
         }
     }
     Ok(())
